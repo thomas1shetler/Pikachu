63f07b0b3c998f7eb5274326f0137071f7a65d2f
thomas1shetler <43040805+thomas1shetler@users.noreply.github.com>
2018-09-07 12:38:46 -0700

    Pikachu.java

---
+++ pikachu.java
@@ -0,0 +1,8 @@
+public class pikatchu {
+  public static void main(String[]args) {
+	  System.out.println("Pikatchu welcomes you to the world of Pokemon!");
+	  System.out.println(" (\\__/)");
+	  System.out.println(" (o^.^)");
+	  System.out.println("z(_(\")(\")");
+  }
+}
